0f1280f6b1e6dc58ba65f7226d4d0ae0e090ba49
Geon Youn <geon.youn@outlook.com>
2023-01-17 14:02:38 -0500

    Create buttons for resizing grid

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-const container = document.querySelector('#container');
+let size = 16;
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.addEventListener('mouseover', e => {
-            e.target.setAttribute('id', 'hovering');
-        });
-        div.addEventListener('mouseout', e => {
-            e.target.removeAttribute('id');
-        });
-        row.appendChild(div);
-    }
-    container.appendChild(row);
+const sizeButton = document.querySelector('#set-size');
+sizeButton.addEventListener('click', e => {
+    size = getUserInput();
+    createGrid();
+});
+
+const createButton = document.querySelector('#create-grid');
+createButton.addEventListener('click', e => {
+    createGrid();
+});
+
+function getUserInput() {
+    let size = 0;
+    do {
+        size = Number(prompt("Enter the size of the grid (max 100):"));
+    } while (size < 0 || size > 100);
+    return size;
 }
+
+function createGrid() {
+    const container = document.querySelector('#grid');
+    container.innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.addEventListener('mouseover', () => {
+                div.setAttribute('id', 'hovering');
+            });
+            // div.addEventListener('mouseout', () => {
+            //     div.removeAttribute('id');
+            // });
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
+};
