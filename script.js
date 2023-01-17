b8e63294b5d95641bc72efae30bf378b10778161
Geon Youn <geon.youn@outlook.com>
2023-01-17 13:46:34 -0500

    Add hovering style

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ for (let i = 0; i < 16; i++) {
     row.classList.add('row');
     for (let j = 0; j < 16; j++) {
         const div = document.createElement('div');
+        div.addEventListener('mouseover', e => {
+            e.target.setAttribute('id', 'hovering');
+        });
+        div.addEventListener('mouseout', e => {
+            e.target.removeAttribute('id');
+        });
         row.appendChild(div);
     }
     container.appendChild(row);
