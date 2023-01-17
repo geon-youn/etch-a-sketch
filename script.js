865052fa96cd3a74dc2227c39a3b1b595499e672
Geon Youn <geon.youn@outlook.com>
2023-01-17 14:36:57 -0500

    Implement dragging instead of hovering

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let size = 16;
+const container = document.querySelector('#grid');
 
 const sizeButton = document.querySelector('#set-size');
 sizeButton.addEventListener('click', e => {
@@ -11,7 +12,7 @@ createButton.addEventListener('click', e => {
     createGrid();
 });
 
-function getUserInput() {
+const getUserInput = () => {
     let size = 0;
     do {
         size = Number(prompt("Enter the size of the grid (max 100):"));
@@ -19,22 +20,37 @@ function getUserInput() {
     return size;
 }
 
-function createGrid() {
-    const container = document.querySelector('#grid');
+const createGrid = () => {
     container.innerHTML = "";
     for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
         for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
-            div.addEventListener('mouseover', () => {
-                div.setAttribute('id', 'hovering');
-            });
-            // div.addEventListener('mouseout', () => {
-            //     div.removeAttribute('id');
-            // });
+            div.classList.add('grid-item');
+            div.addEventListener('mousedown', () => {div.setAttribute('id', 'hovering')});
             row.appendChild(div);
         }
         container.appendChild(row);
     }
 };
+
+const addHovering = (e) => {
+    e.target.setAttribute('id', 'hovering');
+};
+
+container.addEventListener('mousedown', () => {
+    const divs = document.querySelectorAll('.grid-item');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', addHovering);
+    });
+});
+
+container.addEventListener('mouseup', () => {
+    const divs = document.querySelectorAll('.grid-item');
+    divs.forEach(div => {
+        div.removeEventListener('mouseover', addHovering);
+    });
+});
+
+createGrid();
